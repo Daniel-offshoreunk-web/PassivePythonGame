6d730190c3bf331df8f49c8b2f11bc90cb85245e
Daniel-offshoreunk-web <offshore.unk@gmail.com>
2025-09-22 12:07:47 -0500

    Initial commit

---
+++ LoadsClass.py
@@ -0,0 +1,105 @@
+import random
+import time
+from tkinter import *
+import pandas as pd
+
+class loads:
+    def __init__(self):
+        global df
+        #Get Data
+        df = pd.read_csv("GameMainFrame.csv")
+        self.row = 0
+        #Make login window
+        self.tk = Tk()
+        self.tk.title("Login")
+        self.tk.resizable(0,0)
+        self.tk.wm_attributes("-topmost", 1)
+        #Input box creation
+        self.label = Label(self.tk, text="Enter Username")
+        self.label.pack(pady=5)
+        self.input_box = Entry(self.tk, width=50)
+        self.input_box.pack(pady = 5)
+        self.label1 = Label(self.tk, text="Enter password")
+        self.label1.pack(pady=5)
+        self.input_box1 = Entry(self.tk, width=50)
+        self.input_box1.pack(pady=5)
+        self.enter_button1 = Button(self.tk, text="Login", command=self.read_all)
+        self.enter_button1.pack(pady=5)
+        self.create_new_button = Button(self.tk, text="Create New Acount", command=self.create_new_account)
+        self.create_new_button.pack(pady = 5)
+        self.access_granted = False
+        #Start main loop
+        self.tk.mainloop()
+    def read_all(self):
+        global df
+        self.answer = self.input_box.get()
+        print(self.answer)
+        self.answer1 = self.input_box1.get()
+        print(self.answer1)
+        try:
+            self.row = df[df["Username"] == self.answer].index[0]
+            print("It worked!")
+            if self.answer1 == df.loc[self.row, 'Password']:
+                self.tk.destroy()
+                self.access_granted = True
+            else:
+                print("Incorrect Password")
+        except Exception as e:
+            self.fail = e
+            print(self.fail)
+    def create_new_account(self):
+        #Recreate Window
+        self.tk.destroy()
+        self.tk = Tk()
+        self.tk.title("Create New Account")
+        self.tk.resizable(0,0)
+        self.tk.wm_attributes("-topmost", 1)
+        #Input box creation
+        self.label = Label(self.tk, text="Enter Username")
+        self.label.pack(pady=5)
+        self.input_box = Entry(self.tk, width=50)
+        self.input_box.pack(pady = 5)
+        self.label1 = Label(self.tk, text="Enter password")
+        self.label1.pack(pady=5)
+        self.input_box1 = Entry(self.tk, width=50)
+        self.input_box1.pack(pady=5)
+        self.button = Button(self.tk, text="Create", command=self.create_account)
+        self.button.pack(pady = 5)
+        #Main Loop
+        self.tk.mainloop()
+    def create_account(self):
+        self.answer = self.input_box.get()
+        print(self.answer)
+        self.answer1 = self.input_box1.get()
+        print(self.answer1)
+        try:
+            if not df['Username'].eq(self.answer).any() == True:
+                df.loc[len(df)] = [0.0, 0.0, 10, 1, 100, 1, 100, time.time(), 1, self.answer, self.answer1]
+                self.row = len(df) - 1
+                self.access_granted = True
+                self.tk.destroy()
+            else:
+                print("This name is already taken")
+        except Exception as e:
+            print(e)
+    def load_game(self):
+        global df
+        stuff = []
+        for i in range(9):
+            stuff.append(df.iat[self.row,i])
+        return stuff
+    def save_game(self, gameclass):
+        global df
+        print("Saving...")
+        df.iloc[self.row,0] = gameclass.cash
+        df.iloc[self.row,1] = gameclass.cps
+        df.iloc[self.row,2] = gameclass.cps_cost
+        df.iloc[self.row,3] = gameclass.cm
+        df.iloc[self.row,4] = gameclass.cm_cost
+        df.iloc[self.row,5] = gameclass.cv
+        df.iloc[self.row,6] = gameclass.cv_cost
+        df.iloc[self.row,7] = time.time()
+        df.iloc[self.row,8] = gameclass.luck
+        with open("GameMainFrame.csv","w") as f:
+            f.write("Saving...")
+        df.to_csv('GameMainFrame.csv', index=False)
---
+++ Main.py
@@ -0,0 +1,236 @@
+from tkinter import *
+import time
+import random
+import pandas as pd
+from LoadsClass import *
+
+#Format Number Function
+def format_number(num):
+    if abs(num) >= 1_000_000_000_000:
+        return f"{num / 1_000_000_000_000:.2f}T"
+    elif abs(num) >= 1_000_000_000:
+        return f"{num / 1_000_000_000:.2f}B"
+    elif abs(num) >= 1_000_000:
+        return f"{num / 1_000_000:.2f}M"
+    elif abs(num) >= 1_000:
+        return f"{num / 1_000:.2f}K"
+    else:
+        return str(round(num))
+
+class Game:
+    def __init__(self):
+        global l
+        #Main Frame and other
+        self.tk = Tk()
+        self.tk.title("Clicker Game")
+        self.tk.resizable(0,0)
+        self.tk.wm_attributes("-topmost", 1)
+        self.last_click_time = time.time()
+        self.cooldowntime = 0.5
+        self.main_frame = Frame(self.tk)
+        self.main_frame.pack(fill=BOTH, expand=1)
+
+        #Load game data on startup
+        game_data = l.load_game()
+        self.cash = game_data[0]
+        self.cps = game_data[1]
+        self.cps_cost = game_data[2]
+        self.cm = game_data[3]
+        self.cm_cost = game_data[4]
+        self.cv = game_data[5]
+        self.cv_cost = game_data[6]
+        offlinetime = time.time() - game_data[7]
+        offlinecash = offlinetime / 2
+        self.cash += self.cps * offlinecash
+        self.luck = game_data[8]
+        
+        #Cash
+        self.cash_var = StringVar()
+        self.cash_var.set(f"Cash: ${format_number(self.cash)}")
+        self.cash_text = Label(self.main_frame, textvariable=self.cash_var, \
+                                 font=("Impact", 30, "bold"))
+        self.cash_text.pack(side=TOP, fill=X)
+
+        #Container
+        self.canvas_container = Frame(self.main_frame)
+        self.canvas_container.pack(fill=BOTH, expand=1)
+
+        #Canvas
+        self.canvas = Canvas(self.canvas_container, width=500, height=500, highlightthickness=0)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+        #Button
+        self.button_rect = self.canvas.create_rectangle(300, 400, 500, 500, \
+                                                    fill="green", \
+                                                    outline='black')
+        self.button_text = self.canvas.create_text(400, 450, text="Draw",\
+                                                   fill="black", \
+                                                   font=("Impact", 55, "bold"))
+        #Upgrade Cps
+        self.upgrade_button_rect =  self.canvas.create_rectangle(250, 300, 500, 400, fill="green", \
+                                                                 outline="black")
+        self.upgrade_button_text = self.canvas.create_text(375, 350, text='Upgrade CPS',\
+                                                           fill="black", \
+                                                           font=("Impact",25,"bold"))
+        #Upgrade Cps%
+        self.cm_upgrade_button_rect = self.canvas.create_rectangle(0, 300, 250, 400, fill="green", \
+                                                                   outline="black")
+        self.cm_upgrade_button_text = self.canvas.create_text(125, 350, text="Upgrade CPS%",\
+                                                           fill="black", font=("Impact",25,"bold"))
+        #Upgrade Card Value & Luck
+        self.cv_upgrade_button_rect = self.canvas.create_rectangle(0, 200, 250, 300, fill="green", \
+                                                                   outline="black")
+        self.cv_upgrade_button_text = self.canvas.create_text(125, 250, text="""Upgrade
+Gambling""",\
+                                                           fill="black", font=("Impact",25,"bold"))
+        
+        #Results Box
+        self.carddrawn = ""
+        self.result_rect = self.canvas.create_rectangle(0,400,300,500, \
+                                                        fill="white", \
+                                                        outline="black")
+        self.result_text = self.canvas.create_text(150,450, text="", fill="black",\
+                                                   font=("Impact",45, "bold"))
+
+        #Info
+        self.list_frame = Frame(self.canvas, background="#FFFFFF", width=10, height=20)
+        self.canvas.create_window((0, 0), window=self.list_frame, anchor="nw")
+
+        self.scrollbar = Scrollbar(self.list_frame)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        
+        self.info_list = Text(self.list_frame, wrap="word", font=("Comic Sans MS", 12), height=8, width=10, yscrollcommand=self.scrollbar.set)
+        self.info_list.pack(padx=5, pady=5, side=LEFT, fill="both", expand=True)
+
+        self.scrollbar.config(command=self.info_list.yview)
+
+        def on_canvas_configure(event):
+            self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        self.list_frame.bind("<Configure>", on_canvas_configure)
+        
+        #Bind Buttons
+        self.canvas.tag_bind(self.button_rect, "<Button-1>", self.ondraw)
+        self.canvas.tag_bind(self.button_text, "<Button-1>", self.ondraw)
+        self.canvas.tag_bind(self.upgrade_button_rect, "<Button-1>", self.cpsupgrade)
+        self.canvas.tag_bind(self.upgrade_button_text, "<Button-1>", self.cpsupgrade)
+        self.canvas.tag_bind(self.cm_upgrade_button_rect, "<Button-1>", self.cmupgrade)
+        self.canvas.tag_bind(self.cm_upgrade_button_text, "<Button-1>", self.cmupgrade)
+        self.canvas.tag_bind(self.cv_upgrade_button_rect, "<Button-1>", self.cvupgrade)
+        self.canvas.tag_bind(self.cv_upgrade_button_text, "<Button-1>", self.cvupgrade)
+
+        #Save on Exit
+        self.tk.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        # Start the cash update loop
+        self.tk.after(1000, self.update_cash_display)
+
+    def on_closing(self):
+        global l
+        l.save_game(self)
+        self.tk.destroy()
+        print("Game Saved!")
+
+    def cooldown(self):
+        if time.time() - self.last_click_time >= self.cooldowntime:
+            self.last_click_time = time.time()
+            return True
+        else:
+            return False
+    def rarity(self, color, name, number, req, extra):
+        if self.draw_chance > req:
+            self.canvas.itemconfigure(self.result_rect, fill=color)
+            self.canvas.itemconfigure(self.result_text, text=name, fill="black")
+            self.cash += number * self.cv
+            if not extra == '':
+                exec(extra)
+        
+    def ondraw(self, event):
+        if self.cooldown() == True:
+            top_chance = 0
+            for i in range(self.luck):
+                recent_chance = round(random.random() * 100, 5)
+                if recent_chance > top_chance:
+                    top_chance = recent_chance
+            self.draw_chance = top_chance
+            print(top_chance)
+            self.rarity("grey", "Common", 1, 0,'')
+            self.rarity("Lightgreen", "Uncommon", 2, 50,'')
+            self.rarity("Lightblue", "Rare", 7, 75,'')
+            self.rarity("purple", "Epic", 40, 83.5,'')
+            self.rarity("red", "Mythic", 100, 92, '')
+            self.rarity("yellow", "Legendary", 150, 95,'')
+            self.rarity("black", "Unknown I", 700, 98,'self.canvas.itemconfigure(self.result_text, fill="white")')
+            self.rarity("black", "Unknown II", 4000, 99.5, 'self.canvas.itemconfigure(self.result_text, fill="white")')
+            self.rarity("black", "Unknown III", 45000, 99.75, 'self.canvas.itemconfigure(self.result_text, fill="white")')
+            self.rarity("cyan", "Immortal I", 450000, 99.9, '')
+            self.rarity("cyan", "Immortal II", 4500000, 99.925, '')
+            self.rarity("cyan", "Immortal III", 45000000, 99.99, '')
+            self.rarity("white", "Byeond I", 450000000, 99.999, '')
+            self.rarity("white", "Beyond II", 4500000000, 99.99975, '')
+            self.rarity("white", "Beyond III", 45000000000, 99.99999, '')
+            self.cash_var.set(f"Cash: ${format_number(self.cash)}")
+
+    def cpsupgrade(self, event):
+        if self.cooldown() == True:
+            cash_available = self.cash
+            cost_of_next_upgrade = self.cps_cost
+            upgrades_to_buy = 0
+            total_cost = 0
+
+            while cash_available >= cost_of_next_upgrade:
+                cash_available -= cost_of_next_upgrade
+                total_cost += cost_of_next_upgrade
+                cost_of_next_upgrade += 5
+                upgrades_to_buy += 1
+                
+            if upgrades_to_buy > 0:
+                ogcps = self.cps
+                self.cash -= total_cost
+                self.cash_var.set(f"Cash: ${format_number(self.cash)}")
+                self.cps += upgrades_to_buy
+                full_string = f"Cps upgraded from {format_number(ogcps)} to {format_number(self.cps)}."
+                self.info_list.insert("0.0", full_string+'\n')
+            else:
+                full_string = f"You need ${format_number(self.cps_cost)} to upgrade this."
+                self.info_list.insert("0.0", full_string+'\n')
+                
+    def cmupgrade(self, event):
+        if self.cooldown() == True:   
+            if self.cash>=self.cm_cost:
+                self.cash -= self.cm_cost
+                self.cm_cost = self.cm_cost * 2
+                self.cash_var.set(f"Cash: ${format_number(self.cash)}")
+                ogcm = self.cm
+                self.cm += 1
+                full_string = f"Cps% upgraded from {ogcm} to {self.cm}."
+                self.info_list.insert("0.0", full_string+'\n')
+            else:
+                full_string = f"You need ${format_number(self.cm_cost)} to upgrade this."
+                self.info_list.insert("0.0", full_string+'\n')
+
+    def cvupgrade(self, event):
+        if self.cooldown() == True:
+            if self.cash > self.cv_cost or self.cash == self.cv_cost:
+                self.cash -= self.cv_cost
+                self.cv_cost = self.cv_cost * 10
+                self.cash_var.set(f"Cash: ${format_number(self.cash)}")
+                ogcv = self.cv
+                self.cv += 1
+                self.luck *= 2
+                full_string = f"Card value and luck upgraded from {ogcv} to {self.cv}."
+                self.info_list.insert("0.0", full_string+'\n')
+            else:
+                full_string = f"You need ${format_number(self.cv_cost)} to upgrade this."
+                self.info_list.insert("0.0", full_string+'\n')
+    
+    def update_cash_display(self):
+        self.cash += self.cps * self.cm
+        self.cash_var.set(f"Cash: ${format_number(self.cash)}")
+        
+        # Schedule the next call to this function after 1000ms (1 second)
+        self.tk.after(1000, self.update_cash_display)
+
+l = loads()
+if l.access_granted == True:
+    g = Game()
+    g.tk.mainloop()
